b161a94e81a9a1393bca37bd09eb16021c4a552a
Patrick Daly <pmdaly12@gmail.com>
2021-08-15 08:16:46 -0700

    stake mapping and some basic tests running

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -6,25 +6,52 @@ import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openze
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
+  event Stake(address _user, uint256 _amount);
+  mapping(address => uint256) public stakedAmounts;
+  uint256 public deadline; 
+  uint256 public constant threshold = 1 ether;
 
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    deadline = now + 30 seconds;
+  } 
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+      require(msg.value > 0, 'can only deposit positive amounts bruh!');
+      stakedAmounts[msg.sender] += msg.value;
+      emit Stake(msg.sender, msg.value);
+  }
 
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
-
+  function execute() public {
+      // require condition
+      require(now >= deadline, 'must wait until the deadline has passed!');
+      console.log('executing...');
+  }
+ 
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+  function withdraw() public {
+      // if time passed and threshold not met, allow anyone to call withdraw
+      require(now >= deadline, 'must wait until the deadline has passed');
+      require(address(this).balance < threshold, 'minimum threshold met, cannot withdraw');
+      console.log('withdrawing...');
+  }
 
 
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns (uint256) {
+      console.log(deadline - now);
+      if (now > deadline) {
+        return 0;
+      } else {
+        return deadline - now;
+      }
+  }
 
 }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,26 +1,31 @@
 const { ethers } = require("hardhat");
-const { use, expect } = require("chai");
-const { solidity } = require("ethereum-waffle");
+const { expect } = require("chai");
 
-use(solidity);
 
-describe("My Dapp", function () {
-  let myContract;
+const toEther = (amount) => {return ethers.utils.parseEther(amount)};
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
-
-      myContract = await YourContract.deploy();
+describe("Staker Dapp", function () {
+    describe("Staker Contract", function () {
+        before(async function () {
+            this.signers = await ethers.getSigners();
+            this.alice = this.signers[0];
+            this.bob = this.signers[1];
+            this.charlie = this.signers[2];
+            this.StakerFactory = await ethers.getContractFactory("Staker");
+        });
+        beforeEach( async function () {
+            this.staker = await this.StakerFactory.deploy(this.alice.address);
+        });
+        it("stake(): can add funds", async function () {
+            expect(await this.staker.stakedAmounts(this.alice.address)).to.equal(0);
+            await this.staker.stake({'from': this.alice.address, 'value': toEther("0.5")});
+            expect(await this.staker.stakedAmounts(this.alice.address)).to.equal(toEther("0.5"));
+        })
+        it("withdraw(): can't withdraw before deadline", async function () {
+            await this.staker.stakedAmounts(this.alice.address);
+            await expect(this.staker.withdraw({'from': this.alice.address})).to.be.revertedWith("must wait until the deadline has passed");
+        });
     });
-
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
-
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
+    describe("External Contract", function () {
     });
-  });
 });
