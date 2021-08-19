ad0d6f7f8130b18b3b58fcbd7cb0e8e9aedc6ca4
Patrick Daly <pmdaly12@gmail.com>
2021-08-19 00:33:14 -0700

    ownable to contract, tests work again

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,57 +1,68 @@
-pragma solidity >=0.6.0 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "./ExampleExternalContract.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Staker {
 
-  ExampleExternalContract public exampleExternalContract;
-  event Stake(address _user, uint256 _amount);
-  mapping(address => uint256) public stakedAmounts;
-  uint256 public deadline; 
-  uint256 public constant threshold = 1 ether;
+contract Staker is Ownable {
 
-  constructor(address exampleExternalContractAddress) public {
-    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-    deadline = now + 30 seconds;
+  ExampleExternalContract private exampleExternalContract;
+  event Stake(address _user, uint _amount);
+  mapping(address => uint) private stakedAmounts;
+  uint public deadline; 
+  uint public constant threshold = 1 ether;
+  bool public openForWithdraw = false;
+
+  constructor(address _externalAddress) {
+    exampleExternalContract = ExampleExternalContract(_externalAddress);
+    deadline = block.timestamp + 30 seconds;
   } 
 
+  modifier deadlinePassed() {
+      require(block.timestamp <= deadline, "cannot deposit funds after the deadline has passed!");
+      _;
+  }
+
+  function getStakedAmount(address _user) public view onlyOwner returns (uint) {
+      return stakedAmounts[_user];
+  }
+
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  //  ( make sure to add a `Stake(address,uint)` event and emit it for the frontend <List/> display )
   function stake() public payable {
-      require(msg.value > 0, 'can only deposit positive amounts bruh!');
+      require(block.timestamp <= deadline, "cannot deposit funds after the deadline has passed!");
       stakedAmounts[msg.sender] += msg.value;
       emit Stake(msg.sender, msg.value);
   }
 
-
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-  function execute() public {
-      // require condition
-      require(now >= deadline, 'must wait until the deadline has passed!');
-      console.log('executing...');
-  }
- 
-
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-  function withdraw() public {
-      // if time passed and threshold not met, allow anyone to call withdraw
-      require(now >= deadline, 'must wait until the deadline has passed');
-      require(address(this).balance < threshold, 'minimum threshold met, cannot withdraw');
-      console.log('withdrawing...');
-  }
-
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-  function timeLeft() public view returns (uint256) {
-      console.log(deadline - now);
-      if (now > deadline) {
-        return 0;
+  function execute() public deadlinePassed {
+      if (address(this).balance > threshold) {
+        exampleExternalContract.complete{value: address(this).balance}();
       } else {
-        return deadline - now;
+        openForWithdraw = true;
       }
   }
 
+  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+  function withdraw() public payable deadlinePassed {
+      // if time passed and threshold not met, allow anyone to call withdraw
+      require(block.timestamp >= deadline, 'must wait until the deadline has passed');
+      require(openForWithdraw == true, 'minimum threshold met, cannot withdraw');
+      uint amount = stakedAmounts[msg.sender];
+      stakedAmounts[msg.sender] = 0;
+      payable(msg.sender).transfer(amount);
+  }
+
+  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function timeLeft() public view returns (uint) {
+      if (block.timestamp > deadline) {
+        return 0;
+      } else {
+        return deadline - block.timestamp;
+      }
+  }
 }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -11,19 +11,23 @@ describe("Staker Dapp", function () {
             this.alice = this.signers[0];
             this.bob = this.signers[1];
             this.charlie = this.signers[2];
+            this.owner = this.signers[3];
+            this.ExternalFactory = await ethers.getContractFactory("ExampleExternalContract");
             this.StakerFactory = await ethers.getContractFactory("Staker");
         });
         beforeEach( async function () {
-            this.staker = await this.StakerFactory.deploy(this.alice.address);
+            this.extenalContract = await this.ExternalFactory.deploy();
+            this.staker = await this.StakerFactory.deploy(this.owner.address);
         });
         it("stake(): can add funds", async function () {
-            expect(await this.staker.stakedAmounts(this.alice.address)).to.equal(0);
+            expect(await this.staker.getStakedAmount(this.alice.address)).to.equal(0);
             await this.staker.stake({'from': this.alice.address, 'value': toEther("0.5")});
-            expect(await this.staker.stakedAmounts(this.alice.address)).to.equal(toEther("0.5"));
+            expect(await this.staker.getStakedAmount(this.alice.address)).to.equal(toEther("0.5"));
         })
         it("withdraw(): can't withdraw before deadline", async function () {
-            await this.staker.stakedAmounts(this.alice.address);
-            await expect(this.staker.withdraw({'from': this.alice.address})).to.be.revertedWith("must wait until the deadline has passed");
+            await this.staker.stake({'from': this.alice.address, 'value': toEther("0.5")});
+            await expect(this.staker.withdraw({'from': this.alice.address})).to.be.revertedWith(
+                "must wait until the deadline has passed");
         });
     });
     describe("External Contract", function () {
